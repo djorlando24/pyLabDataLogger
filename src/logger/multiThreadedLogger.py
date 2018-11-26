64e25785a7c5c4e8686b433cc90d94a9a25b273f
dduke <daniel.duke@monash.edu>
2018-11-27 00:44:02 +1100

    logging module

---
+++ src/logger/multiThreadedLogger.py
@@ -0,0 +1,24 @@
+"""
+    Main module for spawning device threads and logging to file.
+    
+    @author Daniel Duke <daniel.duke@monash.edu>
+    @copyright (c) 2018 LTRAC
+    @license GPL-3.0+
+    @version 0.0.1
+    @date 27/11/2018
+        __   ____________    ___    ______
+       / /  /_  ____ __  \  /   |  / ____/
+      / /    / /   / /_/ / / /| | / /
+     / /___ / /   / _, _/ / ___ |/ /_________
+    /_____//_/   /_/ |__\/_/  |_|\__________/
+
+    Laboratory for Turbulence Research in Aerospace & Combustion (LTRAC)
+    Monash University, Australia
+"""
+
+from pyLabDataLogger.device import usbDevice
+
+
+def start(dest_file, sample_period=1., devices=[], **kwargs):
+    # usbDevice.load_usb_devices(usbDevicesFound, **kwargs)
+    return 
