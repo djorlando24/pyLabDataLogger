9b0d7ecdf0fa1aeb3458fb8a662242a4a09909bc
dduke <daniel.duke@monash.edu>
2019-03-15 18:55:21 +1100

    fluke USB and v4l2 dummy classes

---
+++ src/device/v4l2Device.py
@@ -0,0 +1,143 @@
+"""
+    Video 4 Linux capture device support (v4l2)
+    
+    @author Daniel Duke <daniel.duke@monash.edu>
+    @copyright (c) 2019 LTRAC
+    @license GPL-3.0+
+    @version 0.0.1
+    @date 15/03/2019
+        __   ____________    ___    ______
+       / /  /_  ____ __  \  /   |  / ____/
+      / /    / /   / /_/ / / /| | / /
+     / /___ / /   / _, _/ / ___ |/ /_________
+    /_____//_/   /_/ |__\/_/  |_|\__________/
+
+    Laboratory for Turbulence Research in Aerospace & Combustion (LTRAC)
+    Monash University, Australia
+"""
+
+from device import device, pyLabDataLoggerIOError
+import numpy as np
+import datetime, time
+
+'''
+try:
+    import specialLibrary
+except ImportError:
+    print "Please install specialLibrary"
+    raise
+'''
+
+########################################################################################################################
+class v4l2Device(device):
+    """ Class providing support for Video4Linux2 (v4l2) video capture streams
+    """
+
+    def __init__(self,params={},quiet=True,**kwargs):
+        self.config = {} # user-variable configuration parameters go here (ie scale, offset, eng. units)
+        self.params = params # fixed configuration paramaters go here (ie USB PID & VID, raw device units)
+        self.driverConnected = False # Goes to True when scan method succeeds
+        self.name = "uninitialized"
+        self.lastValue = None # Last known value (for logging)
+        self.lastValueTimestamp = None # Time when last value was obtained
+        if params is not {}: self.scan(quiet=quiet)
+        
+        return
+
+    # Detect if device is present
+    def scan(self,override_params=None,quiet=False):
+        
+        if override_params is not None: self.params = override_params
+        
+        # ...
+        
+        else: self.activate(quiet=quiet)
+        return
+
+    # Establish connection to device (ie open serial port)
+    def activate(self,quiet=False):
+        
+        # ...
+        
+        self.driverConnected=True
+        
+        # Make first query to get units, description, etc.
+        self.query(reset=True)
+
+        if not quiet: self.pprint()
+        return
+
+    # Deactivate connection to device (close serial port)
+    def deactivate(self):
+        # ...
+        self.driverConnected=False
+        return
+
+    # Apply configuration changes to the driver (subdriver-specific)
+    def apply_config(self):
+        subdriver = self.params['driver'].split('/')[1:]
+        try:
+            assert(self.deviceClass)
+            if self.deviceClass is None: raise pyLabDataLoggerIOError("Could not access device.")
+            
+            # Apply subdriver-specific variable writes
+            if subdriver=='?':
+                #...
+                pass
+            else:
+                raise RuntimeError("I don't know what to do with a device driver %s" % self.params['driver'])
+        
+        except ValueError:
+            print "%s - Invalid setting requested" % self.name
+            print "\t(V=",self.params['set_voltage'],"I=", self.params['set_current'],")"
+        
+        return
+
+    # Update configuration (ie change sample rate or number of samples)
+    def update_config(self,config_keys={}):
+        for key in config_keys.keys():
+            self.config[key]=self.config_keys[key]
+        self.apply_config()
+        return
+
+    def updateTimestamp(self):
+        self.lastValueTimestamp = datetime.datetime.now()
+
+    # Re-establish connection to device.
+    def reset(self):
+        self.deactivate()
+        self.scan()
+        if self.driverConnected: self.activate()
+        else: print "Error resetting %s: device is not detected" % self.name
+
+    
+
+    # Handle query for values
+    def query(self, reset=False):
+
+        # Check
+        try:
+            assert(self.deviceClass)
+            if self.deviceClass is None: raise pyLabDataLoggerIOError
+        except:
+            print "Connection to the device is not open."
+
+        # If first time or reset, get configuration (ie units)
+        if not 'raw_units' in self.params.keys() or reset:
+            driver = self.params['driver'].split('/')[1:]
+            self.subdriver = None #driver[0].lower()
+            # set self.params, self.config...
+
+        # Read values        
+        self.get_values()
+
+        # Generate scaled values. Convert non-numerics to NaN
+        lastValueSanitized = []
+        for v in self.lastValue: 
+            if v is None: lastValueSanitized.append(np.nan)
+            else: lastValueSanitized.append(v)
+        self.lastScaled = np.array(lastValueSanitized) * self.config['scale'] + self.config['offset']
+        self.updateTimestamp()
+        return self.lastValue
+
+
