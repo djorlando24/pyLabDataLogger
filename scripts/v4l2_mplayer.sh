f27a20e99e7e9617386f2a01f935dc24c4cdcc04
dduke <daniel.duke@monash.edu>
2019-03-18 11:46:19 +1100

    scripts clean up

---
+++ scripts/v4l2_mplayer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mplayer tv://device=/dev/video0 -tv norm=NTSC:width=720:height=480
--- scripts/v4l_test.py
+++
@@ -1,2 +0,0 @@
-# This works with my video capture device:
-# mplayer tv://device=/dev/video0 -tv norm=NTSC:width=720:height=480
